9a9abab746cbccba6b3c60d933d1deac6c14d693
JaVD054 <jeedu124@gmail.com>
2022-10-03 10:46:56 +0530

    task2

--- eyrc-2022_holabot/scripts/task_0.py
+++ eyrc-2022_holabot/scripts/task_0.py
@@ -28,65 +28,161 @@
 ####################### IMPORT MODULES #######################
 import sys
 import traceback
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from math import sqrt,pi,atan2
 ##############################################################
-
-
-def callback(data):
-	"""
-	Purpose:
-	---
-	This function should be used as a callback. Refer Example #1: Pub-Sub with Custom Message in the Learning Resources Section of the Learning Resources.
-    You can write your logic here.
-    NOTE: Radius value should be 1. Refer expected output in document and make sure that the turtle traces "same" path.
-
-	Input Arguments:
-	---
-        `data`  : []
-            data received by the call back function
-
-	Returns:
-	---
-        May vary depending on your logic.
-
-	Example call:
-	---
-        Depends on the usage of the function.
-	"""
-
-
-def main():
-	"""
-	Purpose:
-	---
-	This function will be called by the default main function given below.
-    You can write your logic here.
-
-	Input Arguments:
-	---
-        None
-
-	Returns:
-	---
-        None
-
-	Example call:
-	---
-        main()
-	"""
 
 
 ################# ADD GLOBAL VARIABLES HERE #################
 
-
-
-##############################################################
-
-
-################# ADD UTILITY FUNCTIONS HERE #################
-
-
+global linear_vel , angular_vel , init_data , top_data
+linear_vel = 1.0
+angular_vel = 1.0
+bool_linear = False
 
 ##############################################################
+
+class TurtleBot:
+
+    def __init__(self):
+
+        rospy.init_node('turtlesim_node', anonymous=True)
+
+        # Publisher which will publish to the topic '/turtle1/cmd_vel'.
+        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel',
+                                                  Twist, queue_size=10)
+
+        # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
+        # when a message of type Pose is received.
+        self.pose_subscriber = rospy.Subscriber('/turtle1/pose',
+                                                Pose, self.callback)
+
+        self.pose = Pose()
+        self.rate = rospy.Rate(10)
+
+    #callback function
+    def callback(self, data):
+        """Callback function which is called when a new message of type Pose is
+        received by the subscriber."""
+        global bool_linear
+        self.pose = data
+        self.pose.x = round(self.pose.x,4)
+        self.pose.y = round(self.pose.y,4)
+        if data.theta < -pi/2:
+            bool_linear = True
+
+    def euclidean_distance(self, goal_pose):
+        """Euclidean distance between current pose and the goal."""
+        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
+                    pow((goal_pose.y - self.pose.y), 2))
+
+    def linear_vel(self, goal_pose, constant=1.5):
+        return constant * self.euclidean_distance(goal_pose)
+
+    def steering_angle(self, goal_pose):
+        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
+
+    def angular_vel(self, goal_pose, constant=6):
+        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
+
+    def move2goal(self):
+        """Moves the turtle to the goal."""
+        goal_pose = Pose()
+
+        goal_pose.x = self.pose.x
+        goal_pose.y = self.pose.y - 2
+
+        distance_tolerance = 0.00001
+
+        vel_msg = Twist()
+
+        while self.euclidean_distance(goal_pose) >= distance_tolerance:
+
+            # Linear velocity in the x-axis.
+            vel_msg.linear.x = 1
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+
+            # Angular velocity in the z-axis.
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = self.angular_vel(goal_pose)
+            print(f"My turleBot is: Moving straight!!! \n{self.pose.theta:.2f}")
+
+            # Publishing our vel_msg
+            self.velocity_publisher.publish(vel_msg)
+
+            # Publish at the desired rate.
+            self.rate.sleep()
+
+        # Stopping our robot after the movement is over.
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+    
+    def circle(self):
+        global bool_linear
+        goal_pose = Pose()
+        goal_pose.x = self.pose.x
+        goal_pose.y = self.pose.y
+        goal_pose.theta = self.pose.theta
+        vel_msg = Twist()
+        vel_msg.linear.x = linear_vel
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = angular_vel
+        while not bool_linear:
+            print(f"My turleBot is: Moving in a circle!! \n{self.pose.theta:.2f}")
+            self.velocity_publisher.publish(vel_msg)
+            self.rate.sleep()
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+
+    def setDesiredOrientation(self, desired_angle_radians):
+        tolerence = 0.000001
+        vel_msg = Twist()
+        vel_msg.linear.x = 0
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 0
+        if desired_angle_radians-self.pose.theta < 0:
+            vel_msg.angular.z = -abs(angular_vel)
+        else:
+            vel_msg.angular.z = abs(angular_vel)
+        while not (desired_angle_radians-self.pose.theta < tolerence):
+            print(f"My turleBot is: Rotating! \n{self.pose.theta:.2f}")
+            self.velocity_publisher.publish(vel_msg)
+
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+
+        
+
+#main function
+def main():
+    turtle = TurtleBot()
+    # input()
+    turtle.circle()
+    turtle.setDesiredOrientation(-pi/2)
+    turtle.move2goal()
+    rospy.signal_shutdown("Done!!")
+    print("Done!!")
+
+        
+
+
+
+
+
+
+
 
 
 ######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THIS PART #########
